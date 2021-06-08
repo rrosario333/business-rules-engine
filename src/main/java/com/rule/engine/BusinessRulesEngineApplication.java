1b6ebd116531da6909f2ea41c7d499e7a2df207f
robertrosarioselvaraj <robertrosarioselvaraj@gmail.com>
2021-06-08 23:37:50 +0100

    initial commit

---
+++ src/main/java/com/rule/engine/BusinessRulesEngineApplication.java
@@ -0,0 +1,13 @@
+package com.rule.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessRulesEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessRulesEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rule/engine/BusinessRulesEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rule.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessRulesEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
